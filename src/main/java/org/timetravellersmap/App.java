a32b04a31b4d7e58d9610ccbd14935844fc939ed
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-01-23 17:33:50 +0000

    Include App.java

---
+++ src/main/java/org/timetravellersmap/App.java
@@ -0,0 +1,17 @@
+package org.timetravellersmap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    /**
+     * GeoTools Quickstart demo application. Prompts the user for a shapefile and displays its
+     * contents on the screen in a map frame
+     */
+    public static void main(String[] args) throws Exception {
+       Toplevel toplevelGui = new Toplevel();
+       toplevelGui.show();
+    }
+}
