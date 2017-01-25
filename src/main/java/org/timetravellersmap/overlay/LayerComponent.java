31e7881dcf9119415a559bf8da882f6cc15368ad
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-01-25 14:36:24 +0000

    Add worldToScreen function to layerComponent

--- src/main/java/org/timetravellersmap/overlay/LayerComponent.java
+++ src/main/java/org/timetravellersmap/overlay/LayerComponent.java
@@ -5,6 +5,8 @@ import org.geotools.map.MapViewport;
 import org.timetravellersmap.Annotation;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 
 /**
  * Created by joshua on 23/01/17.
@@ -13,5 +15,12 @@ public abstract class LayerComponent {
     private Annotation layerAnnotation;
     public abstract void draw(Graphics2D graphics2D, MapContent mapContent, MapViewport mapViewport);
     public abstract void displayAnnotation();
+
+    protected Point2D.Double worldToScreen(Point2D.Double point, MapContent mapContent, MapViewport mapViewport) {
+        AffineTransform worldToScreenTransform = mapViewport.getWorldToScreen();
+        Point2D.Double result = new Point2D.Double();
+        worldToScreenTransform.transform(point, result);
+        return result;
+    }
 
 }
