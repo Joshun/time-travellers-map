2262299ebdac3754a269f348a474712bcb401c0e
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-01-27 16:16:42 +0000

    Add sqlite dependency to project

--- src/main/java/org/timetravellersmap/sql/SqliteQuery.java
+++ src/main/java/org/timetravellersmap/sql/SqliteQuery.java
@@ -1,11 +1,19 @@
 package org.timetravellersmap.sql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.timetravellersmap.timeline.Event;
 
 /**
  * Created by joshua on 24/01/17.
  */
 public class SqliteQuery extends Query {
+    public SqliteQuery() {
+
+    }
+
     @Override
     public Event readEvent(int eventId) {
         return null;
