73e19c30b2dce5004a9e33d859cd78254ed0928e
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-02-02 12:29:02 +0000

    Write NameAndDescriptionInput

---
+++ src/main/java/org/timetravellersmap/gui/eventpane/NameAndDescriptionInput.java
@@ -0,0 +1,47 @@
+package org.timetravellersmap.gui.eventpane;
+
+import javax.swing.*;
+
+/**
+ * Created by joshua on 02/02/17.
+ */
+public class NameAndDescriptionInput extends JPanel{
+    private JTextField nameField = new JTextField();
+    private JTextArea descriptionArea = new JTextArea();
+    private JCheckBox toggle = new JCheckBox("Name and description?", false);
+    private JPanel subPanel = new JPanel();
+    private boolean subPanelVisible = false;
+
+    public NameAndDescriptionInput() {
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        subPanel.setLayout(new BoxLayout(subPanel, BoxLayout.PAGE_AXIS));
+        add(toggle);
+        subPanel.add(new JLabel("Name:"));
+        subPanel.add(nameField);
+        subPanel.add(new JLabel("Description:"));
+        subPanel.add(descriptionArea);
+        subPanel.setVisible(subPanelVisible);
+        add(subPanel);
+
+        toggle.addActionListener(actionEvent -> {
+            subPanelVisible = !subPanelVisible;
+            subPanel.setVisible(subPanelVisible);
+        });
+    }
+
+    public String getName() {
+        return subPanelVisible ? nameField.getText() : null;
+    }
+
+    public String getDescription() {
+        return subPanelVisible ? descriptionArea.getText() : null;
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.add(new NameAndDescriptionInput());
+        window.setSize(200,200);
+        window.setVisible(true);
+    }
+}
