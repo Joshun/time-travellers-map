7da8d590b53a282067f847ffbb7f45d4b4999ba5
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-02-22 14:58:17 +0000

    Logging in LayerList

--- src/main/java/org/timetravellersmap/overlay/LayerList.java
+++ src/main/java/org/timetravellersmap/overlay/LayerList.java
@@ -6,19 +6,22 @@ import org.timetravellersmap.gui.MapFrame;
 import org.timetravellersmap.core.event.Event;
 
 import java.util.*;
+import java.util.logging.Logger;
 
 /**
  * LayerList: represents a list of layers drawn on the map and their order
- * Currently not used
+ * Has utility functions for adding, removing and changing layer order, and moving an event to a different layer
  */
 public class LayerList {
+    private final static Logger LOGGER = Logger.getLogger(LayerList.class.getName());
 //    private TreeMap<Layer,Integer> layers = new TreeMap<>();
     private ArrayList<Layer> layers = new ArrayList<Layer>();
     private MapContent mapContent;
 
-    private MapFrame mapFrame;
     // This is the default layer, for usability and stability this cannot be removed
     public final static Layer DEFAULT_LAYER = new Layer("Default");
+
+    // Base layer index and base layer, this is the layer containing the map itself
     public final static int BASE_LAYER_INDEX = 0;
     private org.geotools.map.Layer baseLayer = null;
 
@@ -37,11 +40,6 @@ public class LayerList {
         updateMapContent();
     }
 
-//    public void removeLayer(int orderIndex) {
-//
-//        layers.remove(Integer.valueOf(orderIndex));
-//    }
-
     public void removeLayer(Layer layer) {
         if (layer != DEFAULT_LAYER) {
             // Set all of its events to the default layer
@@ -51,27 +49,14 @@ public class LayerList {
             layers.remove(layer);
         }
         else {
-            System.out.println("Refusing request to remove default layer.");
+            LOGGER.warning("Refusing request to remove default layer.");
         }
         updateMapContent();
-//        mapContent.removeLayer(layer);
     }
 
     public void swapLayers(Layer layer1, Layer layer2) {
-        System.out.println("layers " + mapContent.layers());
-        System.out.println("size " + layers.size());
+        LOGGER.info("swapping layers \"" + layer1 + "\" and \"" + layer2 + "\"");
         Collections.swap(layers, layers.indexOf(layer1), layers.indexOf(layer2));
-//
-//        // Swap using geotools mapcontent
-//        List<org.geotools.map.Layer> mapContentLayers = mapContent.layers();
-//        int layer1Index = mapContentLayers.indexOf(layer1);
-//        int layer2Index = mapContentLayers.indexOf(layer2);
-//        int tempLayer1Index = mapContentLayers.size();
-////        mapContent.moveLayer(layer1Index, tempLayer1Index);
-////        mapContent.moveLayer(layer2Index, layer1Index);
-////        mapContent.moveLayer(tempLayer1Index, layer2Index);
-//        mapContent.moveLayer(layer1Index, layer2Index);
-
         updateMapContent();
     }
 
@@ -117,38 +102,22 @@ public class LayerList {
         return layers.size();
     }
 
-    public void draw(MapContent mapContent, ArrayList<Event> currentEvents) {
 
-//        for (Map.Entry<Integer,Layer> layerEntry: layers.entrySet()) {
-//            int orderIndex = layerEntry.getKey().intValue();
-//            System.out.println("Drawing layer " + orderIndex);
-//            Layer layer = layerEntry.getValue();
-//            layer.draw();
-//        }
-    }
-
-//    public void updateMapContent(MapContent mapContent) {
-//        for (Layer layer: layers) {
-//            mapContent.removeLayer(layer);
-//        }
-//        mapContent.addLayers(layers);
-//    }
-
-    // Copies user layers to the mapcontent layers
-    // We start at mapcontent layer index 1 as 0 is the feature (map) layer
-
+    // Removes all layers from rendered list apart from base layer
+    // Called if layers have been removed
     private void purgeMapContent() {
+        LOGGER.info("purgeMapContent() called");
         List<org.geotools.map.Layer> mapContentLayers = mapContent.layers();
-//        org.geotools.map.Layer baseLayer = mapContent.layers().get(0);
-//        mapContentLayers.clear();
-//        mapContentLayers.add(baseLayer);
         int size = mapContentLayers.size();
         for (int n=size-1; n>0; n--) {
             mapContentLayers.remove(n);
         }
     }
 
+    // Copies user layers to the mapcontent layers
+    // We start at mapcontent layer index 1 as 0 is the feature (map) layer
     public void updateMapContent() {
+        LOGGER.info("updateMapContent() called");
         List<org.geotools.map.Layer> mapContentLayers = mapContent.layers();
 
         // If the "managed" list of layers has shrunk to smaller than the mapContentLayers, "purge" mapContent layers
@@ -157,7 +126,6 @@ public class LayerList {
         }
 
         for (int i=0; i<layers.size(); i++) {
-            System.out.println("size="+layers.size()+" i="+i);
             if (i+BASE_LAYER_INDEX+1>=mapContentLayers.size()) {
                 mapContentLayers.add(layers.get(i));
             }
@@ -174,7 +142,7 @@ public class LayerList {
     }
 
     public void moveEventToLayer(Event event, Layer newLayer) {
-        System.out.println("move " + event + " to " + newLayer);
+        LOGGER.info("move event \"" + event + "\" to " + newLayer);
         Layer currentLayer = event.getLayer();
         if (newLayer == currentLayer) {
             return;
