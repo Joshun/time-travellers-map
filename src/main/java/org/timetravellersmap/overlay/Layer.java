f595554cba3d33c1d9de88c832796238cb939a65
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-02-27 12:10:45 +0000

    Clean up Layer class

--- src/main/java/org/timetravellersmap/overlay/Layer.java
+++ src/main/java/org/timetravellersmap/overlay/Layer.java
@@ -9,15 +9,12 @@ import org.timetravellersmap.core.event.Event;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.HashMap;
-import java.util.Set;
 
 /**
  * Layer: representation of a screen annotation drawing layer
  * Keeps track of LayerComponents and their corresponding Events
  */
 public class Layer extends org.geotools.map.DirectLayer {
-//    private HashMap<Event, ArrayList<LayerComponent>>  eventLayerComponents = new HashMap<>();
     private ArrayList<LayerComponent> layerComponentsToDraw = new ArrayList<>();
     private ArrayList<Event> registeredEvents = new ArrayList<>();
     private String name;
@@ -26,33 +23,6 @@ public class Layer extends org.geotools.map.DirectLayer {
     public Layer(String layerName) {
         this.name = layerName;
     }
-
-//    public void addComponent(LayerComponent component, Event associatedEvent) {
-//        ArrayList<LayerComponent> componentArrayList = eventLayerComponents.get(associatedEvent);
-//        if (componentArrayList == null) {
-//            componentArrayList = new ArrayList<>();
-//            componentArrayList.add(component);
-//            eventLayerComponents.put(associatedEvent, componentArrayList);
-//        }
-//        else {
-//            componentArrayList.add(component);
-//        }
-//        fireChanged();
-//        System.out.println(eventLayerComponents);
-//    }
-//
-//    public void addEventLayerComponents(ArrayList<LayerComponent> layerComponents, Event associatedEvent) {
-//        if (layerComponents == null) {
-//            return;
-//        }
-//        eventLayerComponents.put(associatedEvent, layerComponents);
-//    }
-//
-//    public void removeComponent(LayerComponent component, Event associatedEvent) {
-//        ArrayList<LayerComponent> componentArrayList = eventLayerComponents.get(associatedEvent);
-//        componentArrayList.remove(component);
-//        fireChanged();
-//    }
 
     public void registerEvent(Event event) {
         if (! registeredEvents.contains(event)) {
@@ -79,56 +49,13 @@ public class Layer extends org.geotools.map.DirectLayer {
         registeredEvents.clear();
     }
 
-//    public void setEventsToDraw(ArrayList<Event> eventsToDraw) {
-//        this.eventsToDraw = eventsToDraw;
-//        fireChanged();
-//    }
-    
-//    public void clearEventsToDraw() {
-//        this.eventsToDraw = null;
-//        fireChanged();
-//    }
-
     public void draw(Graphics2D graphics2D, MapContent mapContent, MapViewport mapViewport) {
         if (layerComponentsToDraw != null) {
             for (LayerComponent layerComponent: layerComponentsToDraw) {
                 layerComponent.draw(graphics2D, mapContent, mapViewport);
             }
         }
-//        if (eventsToDraw != null) {
-//            for (Event event : eventsToDraw) {
-//                ArrayList<LayerComponent> layerComponentsToDraw = eventLayerComponents.get(event);
-//                if (layerComponentsToDraw == null) {
-//                    continue;
-//                }
-//                for (LayerComponent layerComponent : layerComponentsToDraw) {
-//                    layerComponent.draw(graphics2D, mapContent, mapViewport);
-//                }
-//            }
-//        }
     }
-
-//    @Override
-//    public boolean equals(Object other) {
-//        if (other instanceof Layer) {
-//            return ((Layer) other).getName().equals(this.name);
-//        }
-//        else {
-//            return other.equals(this);
-//        }
-//    }
-
-//    public Set<Event> getAllEvents() {
-//        return eventLayerComponents.keySet();
-//    }
-
-//    public ArrayList<LayerComponent> getEventLayerComponents(Event event) {
-//        return eventLayerComponents.get(event);
-//    }
-
-//    public void removeEventComponents(Event event) {
-//        eventLayerComponents.remove(event);
-//    }
 
     public ArrayList<Event> getRegisteredEvents() {
         return registeredEvents;
