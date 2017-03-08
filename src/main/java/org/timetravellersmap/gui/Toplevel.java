72c25f421ba370bdc3e55b52c9a1559e45a4f05a
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-03-08 18:59:02 +0000

    Add ttm.db to gitignore

--- src/main/java/org/timetravellersmap/gui/Toplevel.java
+++ src/main/java/org/timetravellersmap/gui/Toplevel.java
@@ -43,7 +43,6 @@ public class Toplevel {
     private CoordinateReferenceSystem coordinateReferenceSystem = DefaultGeographicCRS.WGS84;
 
     public Toplevel() throws ShapefileException {
-//        shapeFile = JFileDataStoreChooser.showOpenFile("shp", null);
         shapeFile = new File("/home/joshua/Documents/Computer Science/Dissertation Project/sample data/ne_50m_admin_0_sovereignty.shp");
         if (shapeFile == null) {
             throw new ShapefileException("Failed to parse shapefile");
@@ -60,7 +59,6 @@ public class Toplevel {
         // Create a map content and add our shapefile to it
         mapContent = new MapContent();
         mapContent.setTitle("Time Traveller's Map");
-//        mapContent.getViewport().setCoordinateReferenceSystem(coordinateReferenceSystem);
 
         // Setup coordinate reference system (crs) and ensure initial view covers entire world
         mapContent.getViewport().setBounds(new ReferencedEnvelope(
@@ -70,37 +68,14 @@ public class Toplevel {
                 90.0,
                 coordinateReferenceSystem
         ));
-//        mapContent.getViewport().setMatchingAspectRatio(false);
 
         style = SLD.createSimpleStyle(featureSource.getSchema());
         layer = new FeatureLayer(featureSource, style);
-//        mapContent.addLayer(layer);
 
-        // Testing of custom layering system
-//        LayerList layerList = new LayerList(layer);
-//        org.timetravellersmap.overlay.Layer layer1 = new org.timetravellersmap.overlay.Layer("layer1");
-//        org.timetravellersmap.overlay.Layer defaultLayer = LayerList.DEFAULT_LAYER;
-//        LayerComponent layerComponent1 = new RectangleComponent(-100, 100, 200, 0);
-//        Event wwii = new Event(
-//                new GregorianCalendar(1939, 0, 1),
-//                new GregorianCalendar(1945, 0, 1),
-//                new Descriptor("WWII")
-//        );
-//        LayerComponent layerComponent1 = new PointComponent(20, 20, 5);
-//        LayerComponent layerComponent2 = new PointComponent(151, -33, 5);
-//        defaultLayer.addComponent(layerComponent1, wwii);
-//        defaultLayer.addComponent(layerComponent2, wwii);
-//        mapContent.addLayer(defaultLayer);
-//        ArrayList<Event> eventsToDraw = new ArrayList<>();
-//        eventsToDraw.add(wwii);
-//        defaultLayer.setEventsToDraw(eventsToDraw);
-//        layerList.updateMapContent(mapContent);
-//        layerList.removeLayer(defaultLayer);
     }
 
     public void show() {
         // Display the map
-//        JMapFrame.showMap(mapContent);
         MapFrame.setBaseLayer(layer);
         MapFrame.showMap(mapContent);
     }
