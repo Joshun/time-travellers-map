0f6d42318f4adc31c40248615bc3449c6290ad9b
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-03-13 20:17:38 +0000

    Finish writing EventIndex tests

--- src/test/java/EventIndexTest.java
+++ src/test/java/EventIndexTest.java
@@ -37,6 +37,11 @@ public class EventIndexTest extends TestCase {
                 new Descriptor("WWII", "Second World War")
         );
         eventIndex.addEvent(newEvent);
+
+        // Testing start events on given year
+        ArrayList<Event> startYearEvents = eventIndex.getStartEventsForYear(1939);
+        assertEquals(startYearEvents.size(), 1);
+
         // Testing leftmost bound case
         ArrayList<Event> eventsLeftBound = eventIndex.getPointerEvents(1939);
         assertEquals(eventsLeftBound.size(), 1);
@@ -48,6 +53,22 @@ public class EventIndexTest extends TestCase {
         // Testing centre case
         ArrayList<Event> eventsCentre = eventIndex.getPointerEvents(1942);
         assertEquals(eventsCentre.size(), 1);
+
+        // Testing start counts
+        int startCountPrev = eventIndex.countStartEventsForYear(1938);
+        int startCount = eventIndex.countStartEventsForYear(1939);
+        int startCountNext = eventIndex.countStartEventsForYear(1940);
+        assertEquals(startCountPrev, 0);
+        assertEquals(startCount, 1);
+        assertEquals(startCountNext, 0);
+
+        // Testing end counts
+        int endCountPrev = eventIndex.countEndEventsForYear(1944);
+        int endCount = eventIndex.countEndEventsForYear(1945);
+        int endCountNext = eventIndex.countEndEventsForYear(1946);
+        assertEquals(endCountPrev, 0);
+        assertEquals(endCount, 1);
+        assertEquals(endCountNext, 0);
     }
 
     @Test
@@ -62,9 +83,68 @@ public class EventIndexTest extends TestCase {
         ArrayList<Event> addedList = eventIndex.getStartEventsForYear(1939);
         assertEquals(addedList.size(), 1);
 
+        ArrayList<Event> addedList2 = eventIndex.getPointerEvents(1939);
+        assertEquals(addedList2.size(), 1);;
+
         eventIndex.removeEvent(newEvent);
         ArrayList<Event> removedList = eventIndex.getStartEventsForYear(1939);
         assertEquals(removedList.size(), 0);
+
+        ArrayList<Event> removedList2 = eventIndex.getPointerEvents(1939);
+        assertEquals(removedList2.size(), 0);
+
+        int startCount = eventIndex.countStartEventsForYear(1939);
+        assertEquals(startCount, 0);
+
+        int endCount = eventIndex.countEndEventsForYear(1945);
+        assertEquals(endCount, 0);
+    }
+
+    @Test
+    public void testUpdateEvent() {
+        Event oldEvent = new Event(
+                new GregorianCalendar(2009, 0, 20),
+                new GregorianCalendar(2012, 0, 20),
+                new Descriptor("Barack Obama's First Term")
+        );
+
+        Event newEvent = new Event(
+                new GregorianCalendar(2012, 0, 20),
+                new GregorianCalendar(2017, 0, 20),
+                new Descriptor("Barack Obama's Second Term")
+        );
+
+        // Adding event
+        eventIndex.addEvent(oldEvent);
+        ArrayList<Event> events = eventIndex.getStartEventsForYear(2009);
+        assertEquals(events.size(), 1);
+        assertEquals(events.get(0), oldEvent);
+
+        // Updating event
+        eventIndex.updateEvent(oldEvent, newEvent);
+        ArrayList<Event> updatedEvents1 = eventIndex.getStartEventsForYear(2009);
+        assertEquals(updatedEvents1.size(), 0);
+
+        ArrayList<Event> updatedEvents2 = eventIndex.getStartEventsForYear(2012);
+        assertEquals(updatedEvents2.size(), 1);
+        assertEquals(updatedEvents2.get(0), newEvent);
+    }
+
+    @Test
+    public void testEventSummary() {
+        Event newEvent = new Event(
+                new GregorianCalendar(1939, 8, 1),
+                new GregorianCalendar(1945, 8, 2),
+                new Descriptor("WWII", "Second World War")
+        );
+
+        String summaryNoEvents = eventIndex.generateStartEventSummary(1939);
+        assertEquals(summaryNoEvents, null);
+
+        eventIndex.addEvent(newEvent);
+        String summary = eventIndex.generateStartEventSummary(1939);
+        String correctSummary = "<html><b>" + newEvent.getStartDateAsYear() + "</b><br>" + newEvent.getEventDescriptor().getName() + "<br></html>";
+        assertEquals(summary, correctSummary);
     }
 
 }
