6b299e892231a985755efe019af7f6635cd5ce6f
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2017-04-10 15:21:50 +0100

    System look and feel in App

--- src/main/java/org/timetravellersmap/App.java
+++ src/main/java/org/timetravellersmap/App.java
@@ -15,6 +15,24 @@ public class App
      * Launches the application
      */
     public static void main(String[] args) throws Exception {
+        // Try to set system look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Show WelcomeDialog
         WelcomeDialog welcomeDialog = new WelcomeDialog();
         welcomeDialog.setVisible(true);
     }
